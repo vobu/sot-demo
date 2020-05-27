fe4d8464a69d5a8aca332cf0b38edfebc23a988f
Volker Buzek <volker.buzek@js-soft.com>
2020-05-27 13:47:04 +0200

    feat(ts): add types for code-completion

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,19 +1,26 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-    "use strict"
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller"]
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @yields {typeof sap.ui.core.mvc.Controller}
+     */,
+    (Controller) => {
+        "use strict"
 
-    return Controller.extend("demo.sot.sot.controller.Main", {
-        onInit() {},
+        return Controller.extend("demo.sot.sot.controller.Main", {
+            onInit() {},
 
-        onPeopleSelect(oEvent) {
-            const oListItemSelected = oEvent.getParameters().listItem
-            const sPersonDetail = oListItemSelected.getBindingContext("Backend").getPath()
-            const sModelBasePath = oListItemSelected.getBindingContext("Backend").getModel().sServiceUrl // I know
-            fetch(`${sModelBasePath}${sPersonDetail}?$format=json`)
-                .then((response) => response.json())
-                .then((oPersonDetailData) => {
-                    this.getView().getModel("PeopleDetail").setData(oPersonDetailData)
-                })
-                .catch((err) => console.error(err))
-        }
-    })
-})
+            onPeopleSelect(oEvent) {
+                const oListItemSelected = oEvent.getParameters().listItem
+                const sPersonDetail = oListItemSelected.getBindingContext("Backend").getPath()
+                const sModelBasePath = oListItemSelected.getBindingContext("Backend").getModel().sServiceUrl // I know
+                fetch(`${sModelBasePath}${sPersonDetail}?$format=json`)
+                    .then((response) => response.json())
+                    .then((oPersonDetailData) => {
+                        this.getView().getModel("PeopleDetail").setData(oPersonDetailData)
+                    })
+                    .catch((err) => console.error(err))
+            }
+        })
+    }
+)
