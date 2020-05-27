09a4f6580d5ed8783e1094d01c987c83927ce78f
Volker Buzek <volker.buzek@js-soft.com>
2020-05-27 13:36:24 +0200

    feat: people detail

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -2,6 +2,18 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
     "use strict"
 
     return Controller.extend("demo.sot.sot.controller.Main", {
-        onInit() {}
+        onInit() {},
+
+        onPeopleSelect(oEvent) {
+            const oListItemSelected = oEvent.getParameters().listItem
+            const sPersonDetail = oListItemSelected.getBindingContext("Backend").getPath()
+            const sModelBasePath = oListItemSelected.getBindingContext("Backend").getModel().sServiceUrl // I know
+            fetch(`${sModelBasePath}${sPersonDetail}?$format=json`)
+                .then((response) => response.json())
+                .then((oPersonDetailData) => {
+                    this.getView().getModel("PeopleDetail").setData(oPersonDetailData)
+                })
+                .catch((err) => console.error(err))
+        }
     })
 })
